e3218abf8add3cb0907581db25e8db01a4495872
于海慧 <125227112@qq.com>
2018-05-28 17:47:05 +0800

    数据字典功能开发完毕

--- src/libs/kit.js
+++ src/libs/kit.js
@@ -1,7 +1,18 @@
 import store from '../store/index'
 let kit = {
-  msg:{
-    
+  msg: {
+
+  },
+  inputRules: {
+    required: (value) => !!value || '必填',
+    email: (value) => {
+      const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return pattern.test(value) || 'e-mail格式错误'
+    },
+    digital:(val)=>{
+      const pattern=/^[0-9]*$/
+      return (!!!val||pattern.test(val)) || '必须是数字' 
+    }
   }
 };
 
@@ -28,45 +39,45 @@ kit.oneOf = function (ele, targetArr) {
   }
 };
 
-kit.msg.ok=function(msg){
-  let data={
-    y:'top',
-    mode:'multi-line',
-    color:'success',
-    timeout:3000,
-    text:msg,
-    show:true,
+kit.msg.ok = function (msg) {
+  let data = {
+    y: 'top',
+    mode: 'multi-line',
+    color: 'success',
+    timeout: 3000,
+    text: msg,
+    show: true,
   }
 
-  store.commit('setSnackbar',data);
+  store.commit('setSnackbar', data);
 
 }
 
-kit.msg.info=function(msg){
-  let data={
-    y:'top',
-    mode:'multi-line',
-    color:'info',
-    timeout:3000,
-    text:msg,
-    show:true,
+kit.msg.info = function (msg) {
+  let data = {
+    y: 'top',
+    mode: 'multi-line',
+    color: 'info',
+    timeout: 3000,
+    text: msg,
+    show: true,
   }
 
-  store.commit('setSnackbar',data);
+  store.commit('setSnackbar', data);
 
 }
 
-kit.msg.err=function(msg){
-  let data={
-    y:'top',
-    mode:'multi-line',
-    color:'error',
-    timeout:3000,
-    text:msg,
-    show:true,
+kit.msg.err = function (msg) {
+  let data = {
+    y: 'top',
+    mode: 'multi-line',
+    color: 'error',
+    timeout: 3000,
+    text: msg,
+    show: true,
   }
 
-  store.commit('setSnackbar',data);
+  store.commit('setSnackbar', data);
 
 }
 
--- src/views/sys-manage/dd/dd.vue
+++ src/views/sys-manage/dd/dd.vue
@@ -7,23 +7,23 @@
           <span class="headline">{{title}}</span>
         </v-card-title>
         <v-card-text>
-            <v-form v-model="valid">
+            <v-form v-model="fValid" ref="form" lazy-validation>
                 <v-container grid-list-md>
                   <v-layout wrap>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="dd.dict"  label="字典名"  :disabled="pName!=''" required></v-text-field>
+                        <v-text-field v-model="dd.dict"  label="字典名"  :disabled="pName!=''||opt=='update'" :rules="[rules.required,(v) => !!v&&v.length <= 50 || '最多 50 字符']" :counter="50"></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
                         <v-text-field v-model="pName"  label="上级名字"   disabled></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="dd.name"  label="名字"  required></v-text-field>
+                        <v-text-field v-model="dd.name"  label="名字"  :rules="[rules.required,(v) => !!v&&v.length <= 50 || '最多 50 字符']" :counter="50"></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="dd.val"  label="值"  required></v-text-field>
+                        <v-text-field v-model="dd.val"  label="值"  :rules="[rules.required,(v) => !!v&&v.length <= 50 || '最多 50 字符']" :counter="50"></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="dd.idx"  label="序号"  ></v-text-field>
+                        <v-text-field v-model="dd.idx"  label="序号"  :rules="[rules.digital]"></v-text-field>
                       </v-flex>
                   </v-layout>
                 </v-container>
@@ -32,7 +32,8 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="error darken-1" flat @click.native="dialog = false">关闭</v-btn>
-          <v-btn color="success darken-1" flat @click.native="save" :loading="loading" :disabled="loading">保存</v-btn>
+          <v-btn color="success darken-1" flat @click.native="save" :loading="loading" v-show="opt=='add'" :disabled="loading||!fValid">保存</v-btn>
+          <v-btn color="success darken-1" flat @click.native="update" v-show="opt=='update'" :loading="loading" :disabled="loading||!fValid">保存</v-btn>
         </v-card-actions>
       </v-card>
       </v-dialog>
@@ -56,22 +57,22 @@
               </v-list-tile-content>
               <v-list-tile-action>
                       <v-btn icon @click="getChildren(item)">
-                        <v-icon>visibility</v-icon>
+                        <v-icon color="deep-purple darken-3">visibility</v-icon>
                       </v-btn>
               </v-list-tile-action>
                <v-list-tile-action @click="toAdd(item.name,item.id,item.dict)">
                       <v-btn icon >
-                        <v-icon>add_circle_outline</v-icon>
+                        <v-icon color="blue">add_circle_outline</v-icon>
                       </v-btn>
               </v-list-tile-action>
               <v-list-tile-action>
-                      <v-btn icon >
-                        <v-icon>update</v-icon>
+                      <v-btn icon @click="toEdit(item)">
+                        <v-icon color="teal">edit</v-icon>
                       </v-btn>
               </v-list-tile-action>
               <v-list-tile-action>
-                      <v-btn icon >
-                        <v-icon>delete</v-icon>
+                      <v-btn icon @click="del(item)">
+                        <v-icon color="pink" >delete</v-icon>
                       </v-btn>
               </v-list-tile-action>
             </v-list-tile>
@@ -114,12 +115,14 @@ import Kit from "../../../libs/kit.js";
 export default {
   data() {
     return {
+      fValid: true,
       valid: true,
       loading: false,
       title: "新增数据字典",
       ddListTitle: "字典数据列表",
       pName: "",
       pId: "",
+      rules: Kit.inputRules,
       icon: "menu",
       headers: [
         {
@@ -132,11 +135,11 @@ export default {
         { text: "名字", value: "name" },
         { text: "值", value: "val" },
         { text: "序号", value: "idx" },
-        { text: '操作', value: 'name', sortable: false }
+        { text: "操作", value: "name", sortable: false }
       ],
       dialog: false,
-      opt:'',
-      selectId:'',
+      opt: "",
+      selectId: ""
     };
   },
   computed: {
@@ -147,68 +150,108 @@ export default {
     })
   },
   mounted() {
-    this.refresh()
-    this.$store.commit('setDdChildren',[])
+    this.refresh();
+    this.$store.commit("setDdChildren", []);
   },
 
   methods: {
-    refresh(){
+    refresh() {
       this.$store.dispatch("query_dd_json");
     },
     save() {
-      let vm=this;
+      let vm = this;
       this.loading = true;
-      this.$store
-        .dispatch("save_dd")
-        .then((res) => {
-          this.loading = false;
-          this.dialog=false;
-          if(res.resCode=='success'){
-            if(pName==''){
-              vm.$store.dispatch('query_dd_json');
-            }else{
-              vm.$store.dispatch('get_dd_children',vm.selectId)
+      if (this.$refs.form.validate()) {
+        this.$store
+          .dispatch("save_dd")
+          .then(res => {
+            this.loading = false;
+
+            if (res.resCode == "success") {
+              this.dialog = false;
+              if (vm.pName == "") {
+                vm.$store.dispatch("query_dd_json");
+              } else {
+                vm.$store.dispatch("get_dd_children", { id: vm.selectId });
+              }
             }
-          }
-          
-          
-        })
-        .catch(() => {
-          this.loading = false;
-          this.dialog=false;
-        });
+          })
+          .catch(() => {
+            this.loading = false;
+            this.dialog = false;
+          });
+      }
     },
-    toAdd(pName,pId,dict) {
-      this.opt='add';
-      this.selectId=pId!=undefined?pId:0;
-      this.$store.commit('setDd',{pId:this.selectId,dict:dict});
-      this.pName=pName!=undefined?pName:'';
-      if(this.pName=='')
-        this.title = "新增数据字典";
-      else
-        this.title="新增数据字典值";
-      this.dialog=true;
-
-
+    toAdd(pName, pId, dict) {
+      this.$refs.form.reset();
+      this.opt = "add";
+      this.selectId = pId != undefined ? pId : 0;
+      this.$store.commit("setDd", { pId: this.selectId, dict: dict });
+      this.pName = pName != undefined ? pName : "";
+      if (this.pName == "") this.title = "新增数据字典";
+      else this.title = "新增数据字典值";
+      this.dialog = true;
     },
     getChildren(dd) {
-      // this.ddChildren.splice(0, this.ddChildren.length); //清空数组
-      // dd.children.forEach(function(item, index, array) {
-      //   this.ddChildren.push(item);
-      // });
-      this.ddListTitle='字典【'+dd.name+'】数据列表'
-      this.$store.dispatch('get_dd_children',{id:dd.id})
+      if (dd.id == undefined) this.$store.commit("setDdChildren", []);
+      if (dd.name != undefined)
+        this.ddListTitle = "字典【" + dd.name + "】数据列表";
+      this.$store.dispatch("get_dd_children", { id: dd.id });
+      this.selectId = dd.id;
     },
-    toEdit(dd){
-
+    toEdit(dd) {
+      this.$refs.form.reset();
+      this.opt = "update";
+      this.$store.commit("setDd", dd);
+      this.dialog = true;
+      this.selectId = dd.pId;
+      this.title = dd.pId == 0 ? "修改数据字典" : "修改数据字典值";
     },
-    del(dd){
-
+    update(dd) {
+      let vm = this;
+      this.loading = true;
+      if (this.$refs.form.validate()) {
+        this.$store
+          .dispatch("update_dd")
+          .then(res => {
+            this.loading = false;
+            if (res.resCode == "success") {
+              this.dialog = false;
+              //刷新左侧数据，置空右侧数据
+              if (vm.selectId == 0) {
+                vm.$store.dispatch("query_dd_json");
+                vm.$store.commit("setDdChildren", []);
+              } else {
+                //刷新右侧数据
+                vm.$store.dispatch("get_dd_children", { id: vm.selectId });
+              }
+            }
+          })
+          .catch(() => {
+            this.loading = false;
+            this.dialog = false;
+          });
+      }
+    },
+    del(dd) {
+      let vm = this;
+      this.$APDialog.confirm(function(ret) {
+        if (ret) {
+          vm.$store.dispatch("del_dd", { id: dd.id }).then(res => {
+            if (res.resCode == "success") {
+              if (dd.pId != 0) vm.getChildren({ id: vm.selectId });
+              else {
+                vm.$store.commit("setDdChildren", []);
+                vm.$store.dispatch("query_dd_json");
+              }
+            }
+          });
+        } else {
+        }
+      });
     }
   }
 };
 </script>
 <style>
-
-
 </style>
